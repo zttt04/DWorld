30880b25e9efc43aa7c6291190f23e0c9562187c
ztt <ztt@zttdeMacBook-Pro.local>
2026-05-25 15:57:20 +0800

    Restore carousel controls and resize efficiency figure

--- static/js/index.js
+++ static/js/index.js
@@ -97,8 +97,8 @@ $(document).ready(function() {
 		autoplaySpeed: 5000,
     }
 
-	// Initialize the novel-view carousel only.
-    var carousels = bulmaCarousel.attach('#novel-view-carousel', options);
+	// Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
 	
     bulmaSlider.attach();
     
